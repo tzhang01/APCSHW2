ed3ccc83ae887a546609bfc88edcb3bf3b265028
tzhang01 <tinazhang1197@gmail.com>
2015-02-08 20:50:32 -0500

    made fibonacci method

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -13,12 +13,28 @@ public class Recursion{
 		}
 	}
 
+	public int fib(int n){
+		if(n<0){
+			throw new IllegalArgumentException();
+		}else if(n == 0){
+			return 0;
+		}else if(n== 1){
+			return 1;
+		}else{
+			return fib(n-1) + fib(n-2);
+		}	
+	}
+
 	public static void main(String[]args){
 		Recursion r = new Recursion();
 		System.out.println(r.name());
 		System.out.println(r.fact(4));
 		System.out.println(r.fact(5));
 		//System.out.println(r.fact(-3));
+		System.out.println(r.fib(1));
+		System.out.println(r.fib(0));
+		System.out.println(r.fib(6));
+		System.out.println(r.fib(4));
 	}
 }
 		
