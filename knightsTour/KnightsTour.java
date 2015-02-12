aa27f49f51c9994a7b7d82d8a275072484708cea
Tina Zhang <tinazhang1197@gmail.com>
2015-02-12 12:34:54 -0500

    work in progress

--- knightsTour/KnightsTour.java
+++ knightsTour/KnightsTour.java
@@ -10,6 +10,7 @@ public class KnightsTour{
 
     //instance variable
     private int[][]board;
+    private int size;
        
     //terminal specific character to move the cursor
     private String go(int x,int y){
@@ -30,29 +31,58 @@ public class KnightsTour{
 	for(int i=0;i<board.length;i++){
 	    for(int j=0;j<board[i].length;j++){
 		board[i][j] = -1;
+		ans += " ";
 	    }
+	    ans += "\n";
 	}
-
 	return hide + go(0,0) + ans + "\n" + show;
     }
     
     public KnightsTour(int size){
-	
+	this.size = size;
+	board = new int[size][size];
     } 
     
-    public void solve(){
-	
+    public boolean solve(){
+	board[0][0] = 0;
+	return solve(0, 0, 0);
     }
       
-    public void solve(int startx, int starty){
-				
+    public boolean solve(int startx, int starty){
+	board[startx][starty] = 0;
+	return solve(startx, starty, 0);
     }
       
     public boolean solve(int x,int y,int currentMoveNumber){
+	board[x][y] = currentMoveNumber;
+
 	System.out.println(this);
 	wait(20);
-	
+
+
+	if(currentMoveNumber == size * size -1){
+	    return true;
+	}
+	if(board[x][y] == -1){
+	    if(solve(x+2, y+1, currentMoveNumber+1) ||
+	       solve(x+2, y-1, currentMoveNumber+1) ||
+	       solve(x-2, y+1, currentMoveNumber+1) ||
+	       solve(x-2, y-1, currentMoveNumber+1) ||
+	       solve(x+1, y+2, currentMoveNumber+1) ||
+	       solve(x+1, y-2, currentMoveNumber+1) ||
+	       solve(x-1, y+2, currentMoveNumber+1) ||
+	       solve(x-1, y-2, currentMoveNumber+1)){
+
+		return true;
+	    }
+	    
+	   
 	return false;
     }
       
+}
+
+public static void main(String[]args){
+    KnightsTour b = new KnightsTour(Integer.parseInt(args[0]));
+    
 }
