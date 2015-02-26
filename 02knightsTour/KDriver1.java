551a75b72929eac89afb2e6267ac57b7c45ed673
tzhang01 <tinazhang1197@gmail.com>
2015-02-25 20:32:34 -0500

    knights tour hw

---
+++ 02knightsTour/KDriver1.java
@@ -0,0 +1,8 @@
+public class KDriver1{
+    public static void main(String[]arrr){
+     KnightsTour t = new KnightsTour(5);
+     System.out.println(t.name());
+     System.out.println("Knights Tour:");
+    }
+}
+
---
+++ 02knightsTour/KDriver2.java
@@ -0,0 +1,7 @@
+public class KDriver2{
+	public static void main(String[]arrr){
+		KnightsTour t = new KnightsTour(7);
+     	t.solve();
+     	System.out.println(t);
+    }
+}
