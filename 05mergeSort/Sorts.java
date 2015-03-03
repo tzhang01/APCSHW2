36ef544d3851a3522bfb3e07ce8d4827bc652cf7
tzhang01 <tinazhang1197@gmail.com>
2015-03-03 00:53:52 -0500

    not yet tested

---
+++ 05mergeSort/Sorts.java
@@ -0,0 +1,44 @@
+public class Sorts{
+	private int[]deck;
+	public Sorts(int[]arr){
+		deck = arr;
+	}	
+	public void mergeSort(int[] deck){
+		int[] pile1 = new int [deck.length /2];
+		int[] pile2 = new int [deck.length /2];
+		for(int i=0;i<pile1.length;i++){
+			pile1[i] = deck[i];
+		}
+		for(int i=0;i<pile2.length;i++){
+			pile2[i] = deck[deck.length /2 + i];
+		}
+		mergeSort(pile1, pile2);
+	}
+	public void mergeSort(int[] pile1, int[] pile2){
+		if(pile1.length >1){
+			mergeSort(pile1);
+		}
+		if(pile2.length >1){
+			mergeSort(pile2);
+		}
+		int[]sorted = new int[pile1.length + pile2.length];
+		int place1=0;
+		int place2=0;
+		for(int i=0;i<sorted.length;i++){
+			if(place1 == pile1.length){
+				sorted[i] = pile2[place2++];
+			}else if(place2 == pile2.length){
+				sorted[i] = pile1[place1++];
+			}else{
+				if(pile1[place1] < pile2[place2]){
+					sorted[i] = pile1[place1++];
+				}else{
+					sorted[i] = pile2[place2++];
+			}
+		}
+		deck = sorted;
+	}
+		
+				
+	}
+}
