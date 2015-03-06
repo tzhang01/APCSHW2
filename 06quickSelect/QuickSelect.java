9e68f7280f798381a0facb41a7bd2c59ee51effc
Tina Zhang <tinazhang1197@gmail.com>
2015-03-06 12:33:57 -0500

    start quickSelect

--- 06quickSelect/QuickSelect.java
+++ 06quickSelect/QuickSelect.java
@@ -1,50 +1,42 @@
 import java.util.*;
 
 public class QuickSelect{
-	public static String arrayPrint(int[] ary){
-		String ans = "[ ";
-		for(int i=0;i<ary.length;i++){
-			ans += ary[i] + " ";
-		}
-		return ans + "]";
-	}
-	
-	public static void partition(int[]ary, int si, int ei){
-		//make a new empty array called D of the same size as ARY
-		int[] d = new int[ary.length];
-		//copy all the elements not in the range SI to EI over to the new array
-		for(int i=ei+1;i<ary.length;i++){
-			d[i] = ary[i];
-		}
-		for(int i=si-1;i>=0;i++){
-			d[i] = ary[i];
-		}
+    public static int partition(int[]ary, int si, int ei){
+	int[] d = new int[ary.length];
 
-		//System.out.println(arrayPrint(d));
-		//select a pivot element and pull it out of the arrray. Just choose L[SI] for now and increment SI
-		Random rand = new Random();
-		int pivot = ary[rand.nextInt(ei-si)];
-		//System.out.println(pivot);
-		int r = si;
-		int l = ei;
-		for(int i=si;i<=ei;i++){
-			if(ary[i] < pivot){
-				d[r] = ary[i];
-				r++;
-			}else if(ary[i] > pivot){
-				d[l] = ary[i];
-				l--;
-			}
-			//System.out.println(arrayPrint(d));
-		}
-		d[r] = pivot;
-		//System.out.println(arrayPrint(d));
+	for(int i=ei+1;i<ary.length;i++){
+	    d[i] = ary[i];
+	}
+	for(int i=si-1;i>=0;i++){
+	    d[i] = ary[i];
 	}
 
-	public static void main(String[]args){
-		QuickSelect a = new QuickSelect();
-		int[] test = new int[] {1, 45, 2234, 67, 34 ,7, 8, 24, 9};
-		System.out.println(arrayPrint(test));
-		partition(test, 0, 6);
+	//System.out.println(Arrays.toString(d));
+
+	Random rand = new Random();
+	int pivot = ary[rand.nextInt(ei-si) + si];
+	//	System.out.println(pivot);
+	int end = ei;
+	for(int i=si;i<=end;i++){
+	    if(ary[i] < pivot){
+		d[si] = ary[i];
+		si++;
+	    }else if(ary[i] > pivot){
+		d[ei] = ary[i];
+		ei--;
+	    }
+	    //System.out.println(Arrays.toString(d));
 	}
+	d[si] = pivot;
+	//System.out.println(Arrays.toString(d));      
+	return si;
+    }
+
+
+    public static void main(String[]args){
+	QuickSelect a = new QuickSelect();
+	int[] test = new int[] {1, 45, 2234, 67, 34 ,7, 8, 24, 9};
+	System.out.println(Arrays.toString(test));
+	partition(test, 0, 6);
+    }
 }
