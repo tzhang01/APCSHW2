6b00419a5894f7fa0e912152c678c7c1b61a2269
Tina Zhang <tinazhang1197@gmail.com>
2015-03-09 12:29:44 -0400

    writing partition in place

--- 06quickSelect/QuickSelect.java
+++ 06quickSelect/QuickSelect.java
@@ -2,60 +2,55 @@ import java.util.*;
 
 public class QuickSelect{
     public static int partition(int[]ary, int si, int ei){
-		int[] d = new int[ary.length];
+	Random rand = new Random();
+	int pivot = ary[rand.nextInt(ei-si) + si];
+
+	System.out.println(pivot);
+
 	
-		for(int i=ei+1;i<ary.length;i++){
-		    d[i] = ary[i];
-		}
-		for(int i=si-1;i>=0;i++){
-		    d[i] = ary[i];
-		}
-
-		System.out.println(Arrays.toString(d));
-
-		Random rand = new Random();
-		int pivot = ary[rand.nextInt(ei-si) + si];
+	/*
+	int end = ei;
+	for(int i=si;i<=end;i++){ 
+	    if(ary[i] < pivot){
+		d[si] = ary[i];
+		si++;
+	    }else if(ary[i] > pivot){
+		d[ei] = ary[i];
+		ei--;
+	    }
 		
-		System.out.println(pivot);
-
-		int end = ei;
-		for(int i=si;i<end;i++){ 
-		    if(ary[i] < pivot){
-				d[si] = ary[i];
-				si++;
-		    }else if(ary[i] > pivot){
-				d[ei] = ary[i];
-				ei--;
-		    }
-		
-		System.out.println(Arrays.toString(d));
+	    System.out.println(Arrays.toString(d));
 	
-		}
-		d[si] = pivot;     
-		return si;
-    }
-	public static int quickS(int[] ary, int num){
-		if (num >= ary.length){
-			throw new ArrayIndexOutOfBoundsException();
-		}
-
-		int[] temp = Arrays.copyOf(ary, ary.length);
-		int test = partition(temp, 0, temp.length-1);
-		while(test != num){
-			if(test > num){
-				test = partition(temp, 0, test-1);
-			}else{
-				test = partition(temp, test, temp.length-1);
-			}
-		}
-		return temp[test];
 	}
+	d[si] = pivot;     
+	System.out.println(Arrays.toString(d));
+	return si;
+	*/
+    }
+    public static int quickSelect(int[] ary, int num){
+	int[] temp = Arrays.copyOf(ary, ary.length);
+	System.out.println(Arrays.toString(temp));
+	int test = partition(temp, 0, temp.length-1);
+	while(test != num){
+	    if(test > num){
+		test = partition(temp, 0, test-1);
+	    }else{
+		test = partition(temp, test, temp.length-1);
+	    }
+	}
+	return temp[test];
+    }
 
+    public static void swap(int[] ary, int i, int j){
+	int temp = ary[i];
+	ary[i] = ary[j];
+	ary[j] = temp;
+    }
     public static void main(String[]args){
 	QuickSelect a = new QuickSelect();
 	int[] test = new int[] {1, 45, 2234, 67, 34 ,7, 8, 24, 9};
 	System.out.println(Arrays.toString(test));
 	//partition(test, 0, 6);
-	System.out.println(quickS(test, 3));
+	System.out.println(quickSelect(test, 3));
     }
 }
