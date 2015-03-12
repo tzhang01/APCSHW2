e578d7b690359d9b0c1900ce136df05d1df71bf4
tzhang01 <tinazhang1197@gmail.com>
2015-03-11 23:49:28 -0400

    working add and toString

--- 07linkedList/MyLinkedList.java
+++ 07linkedList/MyLinkedList.java
@@ -1,12 +1,45 @@
 public class MyLinkedList{
     private LNode start;
+	private int sz;
+
     public MyLinkedList(){
+		sz = 0;
     }
-    public boolean add(int value){
-	if(start.getNext() == null){
-	    start = new LNode(value);
-	}else{
-	    ...
+	
+	public String toString(){
+		if(sz == 0){
+			return "[ ]";
+		}
+		String result = "[";
+		LNode now = start;
+		while(now.getNext() != null){
+			result += now + ", ";
+			now = now.getNext();	
+		}
+		result += now;
+		return result + "]";
 	}
+
+    public boolean add(int value){
+		if(sz == 0){
+	   		start = new LNode(value);
+		}else{
+	    	LNode now = start;
+			while(now.getNext() != null){
+				now = now.getNext();
+			}
+			now.setNext(new LNode(value));
+		}
+		sz++;
+		return true;
     }
+	
+
+	public static void main(String args[]){
+		MyLinkedList s = new MyLinkedList();
+		s.add(3);
+		s.add(2);
+		s.add(9);
+		System.out.println(s);
+	}
 }
