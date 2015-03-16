8fdb8170124ba9402480437a6662a83818909a0c
Tina Zhang <tinazhang1197@gmail.com>
2015-03-16 12:32:11 -0400

    making generic

--- 07linkedList/MyLinkedList.java
+++ 07linkedList/MyLinkedList.java
@@ -1,139 +1,143 @@
-public class MyLinkedList{
-    private LNode start;
-	private int sz;
+public class MyLinkedList<T>{
+    private LNode<T> start;
+    private int sz;
+
+    public String name(){
+	return "zhang.tina";
+    }
 
     public MyLinkedList(){
-		sz = 0;
+	sz = 0;
     }
 	
-	public String toString(){
-		if(sz == 0){
-			return "[ ]";
-		}
-		String result = "[";
-		LNode now = start;
-		while(now.getNext() != null){
-			result += now + ", ";
-			now = now.getNext();	
-		}
-		result += now;
-		return result + "]";
+    public String toString(){
+	if(sz == 0){
+	    return "[ ]";
 	}
-
-    public boolean add(int value){
-		boolean done = false;
-		if(sz == 0){
-	   		start = new LNode(value);
-			done = true;
-		}else{
-	    	LNode now = start;
-			while(now.getNext() != null){
-				now = now.getNext();
-			}
-			now.setNext(new LNode(value));
-			done = true;
-		}
-		sz++;
-		return done;
+	String result = "[";
+	LNode<T> now = start;
+	while(now.getNext() != null){
+	    result += now + ", ";
+	    now = now.getNext();	
+	}
+	result += now;
+	return result + "]";
     }
 
-	public void	add(int index, int value){
-		if(index < 0 || index > sz){
-			System.out.println("Index out of Bounds");
-		}
-		if(index == sz){
-			add(value);
-			return;
-		}
-		LNode now = start;
-		while(index != 0){
-			now = now.getNext();
-			index--;
-		}
-		int temp = now.getData();
-		now.setData(value);
-		now.setNext(new LNode(temp, now.getNext()));
-		sz++;
+    public boolean add(T value){
+	boolean done = false;
+	if(sz == 0){
+	    start = new LNode<T>(value);
+	    done = true;
+	}else{
+	    LNode<T> now = start;
+	    while(now.getNext() != null){
+		now = now.getNext();
+	    }
+	    now.setNext(new LNode<T>(value));
+	    done = true;
 	}
+	sz++;
+	return done;
+    }
+
+    public void	add(int index, T value){
+	if(index < 0 || index > sz){
+	    System.out.println("Index out of Bounds");
+	}
+	if(index == sz){
+	    add(value);
+	    return;
+	}
+	LNode<T> now = start;
+	while(index != 0){
+	    now = now.getNext();
+	    index--;
+	}
+	T temp = now.getData();
+	now.setData(value);
+	now.setNext(new LNode<T>(temp, now.getNext()));
+	sz++;
+    }
 		
-	public int get(int index){
-		if(index < 0 || index > sz){
-			System.out.println("Index out of Bounds");
-		}
-		LNode now = start;
-		while(index != 0){
-			now = now.getNext();
-			index--;
-		}
-		return now.getData();
+    public T get(int index){
+	if(index < 0 || index > sz){
+	    System.out.println("Index out of Bounds");
 	}
+	LNode<T> now = start;
+	while(index != 0){
+	    now = now.getNext();
+	    index--;
+	}
+	return now.getData();
+    }
 
-	public int set(int index, int value){
-		if(index < 0 || index > sz){
-			System.out.println("Index out of Bounds");
-		}
-		LNode now = start;
-		while(index != 0){
-			now = now.getNext();
-			index --;
-		}
-		int temp = now.getData();
-		now.setData(value);
-		return temp;
+    public T set(int index, T value){
+	if(index < 0 || index > sz){
+	    System.out.println("Index out of Bounds");
 	}
+	LNode<T> now = start;
+	while(index != 0){
+	    now = now.getNext();
+	    index --;
+	}
+	T temp = now.getData();
+	now.setData(value);
+	return temp;
+    }
 
-	public int remove(int index){
-		if(index < 0 || index > sz){
-			System.out.println("Index out of Bounds");
-		}
-		int temp;
-		if(index == 0){
-			temp = start.getData();
-			start = start.getNext();
-		}else{
-			LNode now = start;
-			while(index != 1){
-				now = now.getNext();
-				index--;
-			}
-			temp = now.getNext().getData();
-			now.setNext(now.getNext().getNext());
-		}
-		sz --;
-		return temp;
+    public T remove(int index){
+	if(index < 0 || index > sz){
+	    System.out.println("Index out of Bounds");
 	}
+	T temp;
+	if(index == 0){
+	    temp = start.getData();
+	    start = start.getNext();
+	}else{
+	    LNode<T> now = start;
+	    while(index != 1){
+		now = now.getNext();
+		index--;
+	    }
+	    temp = now.getNext().getData();
+	    now.setNext(now.getNext().getNext());
+	}
+	sz --;
+	return temp;
+    }
 
-	public int size(){
-		return sz;	
-	}
+    public int size(){
+	return sz;	
+    }
 	
-	public int indexOf(int value){
-		LNode now = start;
-		int index = 0;
-		while(now != null){
-			if(now.getData() == value){
-				return index;
-			}
-			now = now.getNext();
-			index++;
-		}
-		return -1;
+    public int indexOf(T value){
+	LNode<T> now = start;
+	int index = 0;
+	while(now != null){
+	    if(now.getData() == value){
+		return index;
+	    }
+	    now = now.getNext();
+	    index++;
 	}
+	return -1;
+    }
 
-	public static void main(String args[]){
-		MyLinkedList s = new MyLinkedList();
-		s.add(3);
-		s.add(2);
-		s.add(9);
-		System.out.println(s);
-		s.add(1,10);
-		System.out.println(s);
-		System.out.println(s.get(2) + " should return 2");
-		System.out.println(s.set(0, 23));
-		System.out.println(s);
-		System.out.println(s.remove(0));
-		System.out.println(s);
-		System.out.println(s.indexOf(9));
-		System.out.println(s.size());
-	}
+    public static void main(String args[]){
+	MyLinkedList<Integer> s = new MyLinkedList();
+	s.add(3);
+	s.add(2);
+	s.add(9);
+	System.out.println(s);
+	s.add(1,10);
+	System.out.println(s);
+	System.out.println(s.get(2) + " should return 2");
+	System.out.println(s.set(0, 23));
+	System.out.println(s);
+	System.out.println(s.remove(0));
+	System.out.println(s);
+	System.out.println(s.indexOf(9));
+	System.out.println(s.size());
+    }
 }
