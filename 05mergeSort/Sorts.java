9b9956b31c16752d4518d41374399015a372a518
tzhang01 <tinazhang1197@gmail.com>
2015-03-17 00:56:07 -0400

    added name method

--- 05mergeSort/Sorts.java
+++ 05mergeSort/Sorts.java
@@ -1,56 +1,73 @@
+import java.util.*;
 
 public class Sorts{
     private int[]deck;
-    public Sorts(int[]arr){
-	deck = arr;
-    }	
+
+    public String name(){
+	return "zhang.tina";
+    }
+
     public static void main (String[]args){
-	try{
-	    size = args[1];
-	    which = args[0];
-	}catch(ArrayIndexOutOfBoundsException){
-	}
-	int[] start = new int[size];
-	for(int i=0;i<size;i++){
-	    start[i] = 
-    }
-    
-    public void mergeSort(int[] deck){
-	int[] pile1 = new int [deck.length /2];
-	int[] pile2 = new int [deck.length - pile1.length];
-	for(int i=0;i<pile1.length;i++){
-	    pile1[i] = deck[i];
-	}
-	for(int i=0;i<pile2.length;i++){
-	    pile2[i] = deck[deck.length /2 + i];
-	}
-	mergeSort(pile1, pile2);
-    }
-    public void mergeSort(int[] pile1, int[] pile2){
-	if(pile1.length >1){
-	    mergeSort(pile1);
-	}
-	if(pile2.length >1){
-	    mergeSort(pile2);
-	}
-	int[]sorted = new int[pile1.length + pile2.length];
-	int place1=0;
-	int place2=0;
-	for(int i=0;i<sorted.length;i++){
-	    if(place1 == pile1.length){
-		sorted[i] = pile2[place2++];
-	    }else if(place2 == pile2.length){
-		sorted[i] = pile1[place1++];
-	    }else{
-		if(pile1[place1] < pile2[place2]){
-		    sorted[i] = pile1[place1++];
-		}else{
-		    sorted[i] = pile2[place2++];
+/*
+		try{
+	    	int size = (Integer)args[1];
+	    	int which = (Integer)args[0];
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("Type 0 for mergeSort or 1 for Array.out and the size of the array that you want sorted");
 		}
-	    }
-	    deck = sorted;
-	}
-		
-				
+*/
+		int[] start = new int[1000];
+		int[]array = {3,4,77,32,1,3};
+		Random r = new Random();
+		for(int i=0;i<size;i++){
+	    	start[i] = r.nextInt(20000);
+    	}
+		Sorts a = new Sorts();
+
+		System.out.println(start);
+		//if(args[0] == 0){
+			a.mergeSort(start);
+			System.out.println(start);
+		//}else{
+			a.sort(start);
+			System.out.println(start);
+		}
+    }
+    public static void mergeSort(int[] deck){
+		int[] pile1 = new int [deck.length /2];
+		int[] pile2 = new int [deck.length - pile1.length];
+		for(int i=0;i<pile1.length;i++){
+	    	pile1[i] = deck[i];
+		}
+		for(int i=0;i<pile2.length;i++){
+	    	pile2[i] = deck[deck.length /2 + i];
+		}
+		mergeSort(pile1, pile2);
+   	 }
+
+    public static void mergeSort(int[] pile1, int[] pile2){
+		if(pile1.length >1){
+	   		mergeSort(pile1);
+		}
+		if(pile2.length >1){
+	    	mergeSort(pile2);
+		}
+		int[]sorted = new int[pile1.length + pile2.length];
+		int place1=0;
+		int place2=0;
+		for(int i=0;i<sorted.length;i++){
+	    	if(place1 == pile1.length){
+				sorted[i] = pile2[place2++];
+	    	}else if(place2 == pile2.length){
+				sorted[i] = pile1[place1++];
+	    	}else{
+				if(pile1[place1] < pile2[place2]){
+				    sorted[i] = pile1[place1++];
+				}else{
+				    sorted[i] = pile2[place2++];
+				}
+	    	}
+	    	deck = sorted;
+		}				
     }
 }
