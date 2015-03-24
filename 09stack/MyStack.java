64809e37fd0a254de6bc21bec8824f82d62b94a2
tzhang01 <tinazhang1197@gmail.com>
2015-03-24 00:09:55 -0400

    wrote and tested peek method

--- 09stack/MyStack.java
+++ 09stack/MyStack.java
@@ -8,11 +8,14 @@ public class MyStack<T>{
 	public void push(T n){
 		stk.add(0, n);
 	}
+	public T peek(){
+		return stk.get(0);
+	}
 
 	public String toString(){
 		return stk.toString();
 	}
-
+	
 	public static void main(String[]args){
 		MyStack<Integer> q = new MyStack<Integer>();
 		for(int i=0;i<10;i++){
@@ -20,6 +23,7 @@ public class MyStack<T>{
 		}
 
 		System.out.println(q);
+		System.out.println(q.peek());
 	}
 }
 
