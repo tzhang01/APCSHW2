cec8ef8e6924c62aca837938cebe438a791a7053
tzhang01 <tinazhang1197@gmail.com>
2015-03-24 00:06:52 -0400

    wrote and tested push method

---
+++ 09stack/MyStack.java
@@ -0,0 +1,26 @@
+public class MyStack<T>{
+	private MyLinkedList<T> stk;
+	
+	public MyStack(){
+		stk = new MyLinkedList<T>();
+	}
+	
+	public void push(T n){
+		stk.add(0, n);
+	}
+
+	public String toString(){
+		return stk.toString();
+	}
+
+	public static void main(String[]args){
+		MyStack<Integer> q = new MyStack<Integer>();
+		for(int i=0;i<10;i++){
+			q.push(i);
+		}
+
+		System.out.println(q);
+	}
+}
+
+
