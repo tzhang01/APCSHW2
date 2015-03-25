fef0cf1ac80fe9bc457b5f45aa6fdf04f7f0db5d
Tina Zhang <tinazhang1197@gmail.com>
2015-03-25 12:14:48 -0400

    wrote isFull method to check if array is full to help with add methods

--- 10doubleEndedQueue/MyDeque.java
+++ 10doubleEndedQueue/MyDeque.java
@@ -1,4 +1,15 @@
 public class MyDeque<T>{
     private Object[] ary = new Object[10];
-   
+
+    public boolean isFull(Object[] a){
+	for(int i=0;i<a.length;i++){
+	    if(a[i] == null){
+		return false;
+	    }
+	}
+	return true;
+    }
+
+    //public void addFirst(T value){
+	
 }
