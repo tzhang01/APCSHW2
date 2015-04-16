d06a1f961b7d541b8769db0ffdd66eac60a7bf5e
tzhang01 <tinazhang1197@gmail.com>
2015-04-15 23:48:53 -0400

    wrote setPrevious method for coordinate class

--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -12,83 +12,91 @@ public class Maze{
     private int maxx, maxy;
     private int startx, starty;
 
-    public Maze(String filename){
-	startx = -1;
-	starty = -1;
-	String ans = "";
-	try{
-	    Scanner in = new Scanner(new File(filename));
-	    while(in.hasNext()){
-		String line = in.nextLine();
-		if(maxy == 0){
-		    maxx = line.length();
+	private class Coordinate{
+		private int row, col;
+		private Coordinate previous;
+
+		public Coordinate(int row, int col){
+		    this.row = row;
+		    this.col = col;
 		}
-		maxy++;
-		ans+= line;
-	    }
-	}catch(Exception e){
-	    System.out.println("File: " +filename+" could not be opened");
-	    e.printStackTrace();
-	    System.exit(0);
-	}
-	
-	maze = new char[maxx][maxy];
-	for(int i=0;i<ans.length();i++){
-	    char c = ans.charAt(i);
-	    maze[i%maxx][i/maxx] = c;
-	    if(c=='S'){
-		startx = i%maxx;
-		starty = i/maxx;
-	    }
-	}
-    }
-  
-    private class Coordinate{
-	private int row, col;
-	private Coordinate previous;
-	public Coordinate(int row, int col){
-	    this.row = row;
-	    this.col = col;
-	}
+		
+		public void setPrev(Coordinate cor){
+			previous = cor;
+		}
     }
    
     private class Frontier{
-	int mode;
-	private MyDeque<Coordinate> todo =  new MyDeque<Coordinate>();
-	public Frontier(int mode){
-	    this.mode = mode;
-	}
-	public void add(int mode, Coordinate value){
-	    if(mode == 1){ //1 means BSF
-		todo.addLast(value);
-	    }
-	    else{
-		todo.addFirst(value);
-	    }
-	}
-	public Coordinate remove(int mode){
-	    if(mode == 1){
-		return todo.removeFirst();
-	    }
-	    return todo.removeLast();
-	}
+		int mode;
+		private MyDeque<Coordinate> todo =  new MyDeque<Coordinate>();
+
+		public Frontier(int mode){
+		    this.mode = mode;
+		}
+
+		public void add(int mode, Coordinate value){
+			if(mode == 1){ //1 means BSF
+				todo.addLast(value);
+	    	}
+	    	else{
+				todo.addFirst(value);
+	    	}
+		}
+	
+		public Coordinate remove(int mode){
+		    if(mode == 1){
+				return todo.removeFirst();
+		    }
+		    return todo.removeLast();
+		}
     }
+
+	public Maze(String filename){
+		startx = -1;
+		starty = -1;
+		String ans = "";
+		try{
+	    	Scanner in = new Scanner(new File(filename));
+		    while(in.hasNext()){
+				String line = in.nextLine();
+				if(maxy == 0){
+			   		maxx = line.length();
+				}
+				maxy++;
+				ans+= line;
+		    }
+		}catch(Exception e){
+	    	System.out.println("File: " +filename+" could not be opened");
+	    	e.printStackTrace();
+	    	System.exit(0);
+		}
+	
+		maze = new char[maxx][maxy];
+		for(int i=0;i<ans.length();i++){
+		    char c = ans.charAt(i);
+		    maze[i%maxx][i/maxx] = c;
+		    if(c=='S'){
+				startx = i%maxx;
+				starty = i/maxx;
+	    	}
+		}
+	}
     
     public String toString(){
-	String ans = "" + maxx + ", " + maxy + "\n";
-	for(int i=0; i<maxx*maxy;i++){
-	    if(i%maxx == 0 && i != 0){
-		ans += "\n";
-	    }
-	    ans += maze[i%maxx][i/maxx];
-	}
-	return ans;
+		String ans = "" + maxx + ", " + maxy + "\n";
+		for(int i=0; i<maxx*maxy;i++){
+	   		if(i%maxx == 0 && i != 0){
+				ans += "\n";
+	    	}
+	    	ans += maze[i%maxx][i/maxx];
+		}
+		return ans;
     }
     public String toString(boolean animate){
-	if(animate){
-	    return hide + "[0;0H" + toString() + "\n" + show;
-	}
-	return toString();
+		if(animate){
+	 	   return hide + "[0;0H" + toString() + "\n" + show;
+		}
+		return toString();
     }
 
 /*
