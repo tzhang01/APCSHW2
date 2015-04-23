0e5f7d13330c4af530e08bc1524400ca2ce02cb5
Tina Zhang <tinazhang1197@gmail.com>
2015-04-23 12:08:19 -0400

    template for all of the methods that I will have to write

---
+++ 12TreeLab/TreeNode.java
@@ -0,0 +1,31 @@
+public class TreeNode<T>{
+    private TreeNode<T> left;
+    private TreeNode<T> right;
+    private T data;
+    
+    public TreeNode(TreeNode left, TreeNode right, T data){
+
+    }
+
+    public void setLeft(TreeNode l){
+
+    }
+
+    public TreeNode getLeft(){
+	return;
+    }
+
+    public void setRight(TreeNode r){
+    }
+
+    public TreeNode getRight(){
+	return;
+    }
+
+    public void setData(T d){
+    }
+
+    public T getData(){
+	return;
+    }
+}
