f8672896daa935560b0cdee52e5eee6d3a467d95
Tina Zhang <tinazhang1197@gmail.com>
2015-05-12 12:09:37 -0400

    outline of methods i need to implement

---
+++ 13Heaps/MyHeap.java
@@ -0,0 +1,17 @@
+public class MyHeap{
+    public MyHeap(){
+    }
+    public MyHeap(boolean isMax){
+    }
+    public String toString(){
+	return "";
+    }
+    public int remove(){
+	return 0;
+    }
+    public void add(int value){
+    }
+    public int peek(){
+	return 0;
+    }
+}
