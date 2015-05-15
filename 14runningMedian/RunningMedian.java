6e83eb0a65a3ce49a6c5bca8b5a324914c52b2a8
Tina Zhang <tinazhang1197@gmail.com>
2015-05-15 12:33:57 -0400

    compared value and added to appropriate heap in add method

--- 14runningMedian/RunningMedian.java
+++ 14runningMedian/RunningMedian.java
@@ -5,12 +5,21 @@ public class RunningMedian{
     MyHeap large = new MyHeap(); //min heap that holds the larger half
 
     public double getMedian(){
-	if(small[0] > large[0]){
+	if(small.size() > large.size()){
 	    return small.remove();
-	}else if(small[0] < large[0]){
+	}else if(small.size() < large.size()){
 	    return large.remove();
 	}else{
 	    return (large.remove() + small.remove()) / 2;
+	}
+    }
+
+    public void add(int value){
+	//compare value with median and adds it to appropriate side
+	if(value > getMedian()){
+	    large.add(value);
+	}else{
+	    small.add(value);
 	}
     }
 }
